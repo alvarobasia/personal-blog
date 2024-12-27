d6ced6ef5139cdf70282230825b224716b4914d8
Álvaro Basílio <alvarobdaraujo@outlook.com>
2024-12-27 00:11:24 -0300

    feat(workflows): update Lighthouse CI configuration and rename lint job

--- lighthouserc.js
+++ lighthouserc.js
@@ -1,5 +1,10 @@
 module.exports = {
   ci: {
+    collect: {
+      staticDistDir: './next',
+      url: ['http://localhost:3000'],
+      startServerCommand: 'npm run start',
+    },
     upload: {
       target: 'temporary-public-storage',
     },
