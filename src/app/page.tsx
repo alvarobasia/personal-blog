8563c8309227ca2f0bb372ffc588b6cea4cfd9d0
Álvaro Basílio <alvarobdaraujo@outlook.com>
2024-12-28 00:46:38 -0300

    feat(home): add LatestPosts component to display recent blog posts

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,9 +1,11 @@
 import Header from "@/components/header";
+import LatestPosts from "@/components/latest-posts";
 
 export default function Home() {
   return (
     <div className="w-[100vw] h-[100vh] text-foreground p-0 m-0">
       <Header />
+      <LatestPosts />
     </div>
   );
 }
